7d9988f3f826865a7115253522155a0c66b6b636
Pedro Pedro <pedro.afonso.pedro.pp@gmail.com>
2023-12-12 00:37:38 +0000

    vue changes
    added v-ifs
    new buttons

--- vue/src/components/Transactions.vue
+++ vue/src/components/Transactions.vue
@@ -1,61 +1,61 @@
 <script setup>
 
-  import axios from 'axios'
-  import { ref, onMounted } from 'vue'
+import axios from 'axios'
+import { ref, onMounted } from 'vue'
 
-  const transactions = ref([])
+const transactions = ref([])
 
-  const newTransactionParams = ref({
-    id: '',   //como chegar ao metodo do lara que gera next id? 
-    vcard: '', 
-    date: '', 
-    datetime: '',
-    type: '',
-    old_balance:'',
-    new_balance: '', 
-    payment_type: '',
-    payment_reference: '',
-    pair_transaction: '',
-    pair_vcard: '',
-    category_id: '',
-    description: ''
-  })
+const newTransactionParams = ref({
+  id: '',   //como chegar ao metodo do lara que gera next id? 
+  vcard: '',
+  date: '',
+  datetime: '',
+  type: '',
+  old_balance: '',
+  new_balance: '',
+  payment_type: '',
+  payment_reference: '',
+  pair_transaction: '',
+  pair_vcard: '',
+  category_id: '',
+  description: ''
+})
 
-  const loadTransactions = async () => {
-    try {
-      const response = await axios.get('transactions')
-      console.log(response)
-      transactions.value = response.data.data
-    } catch (error) {
-      console.error(error)
-    }
+const loadTransactions = async () => {
+  try {
+    const response = await axios.get('transactions')
+    console.log(response)
+    transactions.value = response.data.data
+  } catch (error) {
+    console.error(error)
   }
+}
 
 
 
-  const emit = defineEmits(['registerNewTransaction'])
+const emit = defineEmits(['registerNewTransaction'])
 
 
-  const registerNewTransaction = async () => {
-    try {
-      const response = await axios.post('transactions', newTransactionParams.value)
-      console.log(response)
-      // here we have to find a way to call http patch requests to update vcards balance
-      // const response = await axios.patch('vcards/' + newTransactionParams.value.vcard, newTransactionParams.value.new_balance)
-      toast.success('Transaction ' + newTransactionParams.value.id + ' completed successfully.')
-      emit('registerNewTransaction')
-      router.back()
-    } catch (error) {
-      console.log(error)
-      toast.error('error while registering!')
-    }
+const registerNewTransaction = async () => {
+  try {
+    const response = await axios.post('transactions', newTransactionParams.value)
+    console.log(response)
+    // here we have to find a way to call http patch requests to update vcards balance
+    // const response = await axios.patch('vcards/' + newTransactionParams.value.vcard, newTransactionParams.value.new_balance)
+    toast.success('Transaction ' + newTransactionParams.value.id + ' completed successfully.')
+    emit('registerNewTransaction')
+    router.back()
+  } catch (error) {
+    console.log(error)
+    toast.error('error while registering!')
   }
+}
 
 
 
-  onMounted(() => {
-    loadTransactions()
-  })
+onMounted(() => {
+  loadTransactions()
+})
 
 </script>
 
@@ -74,15 +74,27 @@
       </thead>
       <tbody>
         <tr v-for="transaction in transactions" :key="transactions.id">
-          <th scope="row">{{ transactions.id}}</th>
+          <th scope="row">{{ transactions.id }}</th>
           <td>{{ transaction.value }}</td>
           <td>{{ transaction.payment_type }}</td>
           <td>{{ transaction.payment_reference }}</td>
           <td>{{ transaction.type }}</td>
-          <td>{{ transaction.description }}</td> 
+          <td>{{ transaction.description }}</td>
         </tr>
       </tbody>
     </table>
   </div>
+  <nav>
+    <ul class="pagination">
+      <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+      <li class="page-item"><a class="page-link" href="#">1</a></li>
+      <li class="page-item"><a class="page-link" href="#">2</a></li>
+      <li class="page-item"><a class="page-link" href="#">3</a></li>
+      <li class="page-item"><a class="page-link" href="#">Next</a></li>
+    </ul>
+  </nav>
 </template>
+
+
+
 
--- vue/src/components/Vcards.vue
+++ vue/src/components/Vcards.vue
@@ -1,23 +1,23 @@
 <script setup>
 
-  import axios from 'axios'
-  import { ref, onMounted } from 'vue'
+import axios from 'axios'
+import { ref, onMounted } from 'vue'
 
-  const vCards = ref([])
+const vCards = ref([])
 
-  const loadVcards = async () => {
-    try {
-      const response = await axios.get('vcards')
-      console.log(response)
-      vCards.value = response.data.data
-    } catch (error) {
-      console.error(error)
-    }
+const loadVcards = async () => {
+  try {
+    const response = await axios.get('vcards')
+    console.log(response)
+    vCards.value = response.data.data
+  } catch (error) {
+    console.error(error)
   }
+}
 
-  onMounted(() => {
-    loadVcards()
-  })
+onMounted(() => {
+  loadVcards()
+})
 
 </script>
 
@@ -31,17 +31,22 @@
           <th scope="col">Name</th>
           <th scope="col">Email</th>
           <th scope="col">Balance</th>
+          <th scope="col"></th>
+          <th scope="col"></th>
         </tr>
       </thead>
       <tbody>
         <tr v-for="vCard in vCards" :key="vCard.phone_number">
-          <th scope="row">{{ vCard.phone_number}}</th>
+          <th scope="row">{{ vCard.phone_number }}</th>
           <td>{{ vCard.name }}</td>
           <td>{{ vCard.email }}</td>
-          <td>{{ vCard.balance}}</td>
+          <td>{{ vCard.balance }}</td>
+          <td><button>Editar</button></td>
+          <td><button>Eliminar</button></td>
         </tr>
       </tbody>
     </table>
   </div>
+  
 </template>
 
