2494ee3849158912ad1bbfc4c692e0d268120a06
NAFNAF <2200685@my.ipleiria.pt>
2023-12-18 04:48:33 +0000

    statistics update

--- vue/src/components/Statistics.vue
+++ vue/src/components/Statistics.vue
@@ -1,7 +1,7 @@
 <script setup>
-import { ref, inject, onMounted } from 'vue'
+import { ref, inject, onMounted } from 'vue';
 import { useUserStore } from "@/stores/user.js";
-import { useRouter } from "vue-router"
+import { useRouter } from "vue-router";
 
 const axios = inject("axios");
 const userStore = useUserStore();
@@ -9,29 +9,51 @@ const router = useRouter();
 
 const vCardStats = ref(null);  // Para armazenar as estatísticas dos vCards
 const isAdmin = ref(false);
+const totalVCardBalance = ref(0);  // Para armazenar o total do saldo de vCards
 
 const fetchVCardStats = async () => {
     try {
-          const response = await axios.get("transactions");
-        //const response = await axios.get(`vcards/${userStore.user?.customer[0].id}/stats`);
+        const response = await axios.get("transactions");
         vCardStats.value = response.data;
     } catch (error) {
         console.error(error);
     }
 };
 
-onMounted(() => {
+const calculateTotalVCardBalance = async () => {
+    try {
+        const response = await axios.get('vcards');
+        const vCards = response.data.data;
+
+        let totalBalance = 0;
+
+        for (const vCard of vCards) {
+            // Certifique-se de que o saldo é um número antes de somar
+            if (!isNaN(vCard.balance)) {
+                totalBalance += parseFloat(vCard.balance);
+            }
+        }
+
+        totalVCardBalance.value = totalBalance.toFixed(2); // Arredonda para duas casas decimais
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+
+onMounted(async () => {
     if (!userStore.user) {
         router.push('/stats');
     } else {
         userStore.loadUser();
 
         // Verifica se o utilizador é um administrador
-        isAdmin.value = userStore.user.user_type
+        isAdmin.value = userStore.user.user_type;
 
         // Carrega estatísticas
         if (isAdmin.value == "A") {
-            fetchVCardStats();
+            await fetchVCardStats();
+            await calculateTotalVCardBalance();
         }
     }
 });
@@ -39,20 +61,15 @@ onMounted(() => {
 
 <template>
     <div>
-      <h1>Estatísticas</h1>
-
+        <h1>Estatísticas</h1>
         <div>
-          <h2>Estatísticas para vCards</h2>
-          <div v-if="vCardStats !== null && vCardStats !== undefined">
-            <h6>Contagem atual de vCards ativos: {{ vCardStats.activeVCardCount }}</h6>
-            <h6>Soma dos saldos atuais de vCard: {{ vCardStats.totalVCardBalance !== null && vCardStats.totalVCardBalance !== undefined ? vCardStats.totalVCardBalance : 'N/A' }}</h6>
-            <h6>Contagem ou soma de todas as transações em um período específico: {{ vCardStats.transactionsInTimeFrame }}</h6>
-            <h6>Total de transações por tipo de pagamento: {{ vCardStats.transactionsByPaymentType }}</h6>
-          </div>
-          <div v-else>
-            <p>Carregando estatísticas...</p>
-          </div>
+            <h2>Estatísticas para vCards</h2>
+            <div v-if="vCardStats !== null && vCardStats !== undefined">
+                <h6>Contagem de balanço total: {{ totalVCardBalance }}€</h6>
+            </div>
+            <div v-else>
+                <p>Carregando estatísticas...</p>
+            </div>
         </div>
     </div>
-  </template>
-  
+</template>
